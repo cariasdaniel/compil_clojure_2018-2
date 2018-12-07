0ead1331049fc012970a311cb7bd6dcc93d1e8c2
cariasdaniel <d.carias@ymail.com>
2018-12-07 10:14:13 -0200

    Add files via upload

---
+++ comp_clojure_p3/project.clj
@@ -0,0 +1,12 @@
+(defproject comp_clojure "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+				[clj-http "2.0.0"]
+				[instaparse "1.4.9"]
+				[org.clojure/core.match "0.3.0-alpha5"]]
+  :main ^:skip-aot comp_clojure.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ comp_clojure_p3/test/comp_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns comp-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [comp-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
